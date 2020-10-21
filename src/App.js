bb77e1f1e5e5dc097863b7f9562960a9049504ef
B-Mugnol <bruno_mugnol@hotmail.com>
2020-10-21 14:07:17 -0300

    Ajuste layout do App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Filter from './components/Filter'
+import Products from './components/Products'
+import ShoppingCart from './components/ShoppingCart'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MotherDiv = styled.div`
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+  display: grid;
+  grid-template-columns: 20% 1fr 20%;
+  column-gap: 8px;
+  text-align: center;
+  padding: 16px 8px;
+  border: 1px dotted black;
+`
+
+const ComponentDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  border: 1px dotted black;
+  padding: 16px 0;
+`
+
+class App extends React.Component {
+  state = {
+    productsArray: []
+  }
+
+
+  render() {
+    return (
+      <MotherDiv>
+        <ComponentDiv><Filter /></ComponentDiv>
+        <ComponentDiv><Products /></ComponentDiv>
+        <ComponentDiv><ShoppingCart /></ComponentDiv>
+      </MotherDiv>
+    );
+  }
 }
 
 export default App;
